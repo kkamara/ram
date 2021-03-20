4ae49ab1e75ae95ae56990299370a6d6dd950bb1
kkamara <kelvinkamara@protonmail.com>
2021-03-20 00:23:18 +0000

    Update APP_URL for local

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,3 +1,3 @@
-export const APP_URL = "https://mortynrick.herokuapp.com/api/v1";
+export const APP_URL = "http://127.0.0.1:3000/api/v1";
 export const APP_NAME = "Rick & Morty";
 export const GIPHY_API_KEY = "M5Pj4m4sIMVxGzJvOEOHwWbzl5UcOKjE";
