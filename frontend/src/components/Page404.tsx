56245a35dcac24c66b723c0ddc7aa9826c08e1d8
kkamara <kelvinkamara@protonmail.com>
2022-03-30 09:17:00 +0100

    Update giphy api usage after breaking change

--- frontend/src/components/Page404.tsx
+++ frontend/src/components/Page404.tsx
@@ -23,7 +23,7 @@ class Page404 extends React.Component {
         )
             .then(res => res.json())
             .then(res => {
-                return res.data.image_original_url
+                return res.data.images.original.url
             })
             .catch(err => {
                 return false
